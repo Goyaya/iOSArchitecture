af29e24d2db527e9b8a297260b2c027bf9405b07
Gyang <goyaya@yeah.net>
2019-07-29 23:41:23 +0800

    base env

---
+++ iOS Knowledge Architecture/MasterTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterTableViewController.h
+//  iOS Knowledge Architecture
+//
+//  Created by 高洋 on 2019/7/29.
+//  Copyright © 2019 Gaoyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MasterTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
