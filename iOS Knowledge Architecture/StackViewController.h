dad8c9ae651701f743b90d506846ef975d3d7112
Gyang <goyaya@yeah.net>
2019-08-16 16:54:56 +0800

    stack view

---
+++ iOS Knowledge Architecture/StackViewController.h
@@ -0,0 +1,17 @@
+//
+//  StackViewController.h
+//  iOS Knowledge Architecture
+//
+//  Created by 高洋 on 2019/8/16.
+//  Copyright © 2019 Gaoyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StackViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS Knowledge Architecture/StackViewController.m
@@ -0,0 +1,24 @@
+//
+//  StackViewController.m
+//  iOS Knowledge Architecture
+//
+//  Created by 高洋 on 2019/8/16.
+//  Copyright © 2019 Gaoyang. All rights reserved.
+//
+
+#import "StackViewController.h"
+
+@interface StackViewController ()
+
+@end
+
+@implementation StackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end
